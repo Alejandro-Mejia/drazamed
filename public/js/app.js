755caa2bd807959068f851f89759a5f96390b1aa
Álvaro Stagg <alvarostagg@protonmail.com>
2020-05-08 23:38:42 -0500

    Added products section in main

--- public/js/app.js
+++ public/js/app.js
@@ -49709,20 +49709,32 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/search-form.scss":
+/*!*****************************************!*\
+  !*** ./resources/sass/search-form.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!**************************************************************************************************************************************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/header.scss ./resources/sass/index.scss ./resources/sass/footer.scss ./resources/sass/login.scss ./resources/sass/drazamed.scss ***!
-  \**************************************************************************************************************************************************************************************************************/
+/*!************************************************************************************************************************************************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/header.scss ./resources/sass/index.scss ./resources/sass/footer.scss ./resources/sass/login.scss ./resources/sass/drazamed.scss ./resources/sass/search-form.scss ***!
+  \************************************************************************************************************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\sass\header.scss */"./resources/sass/header.scss");
-__webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\sass\index.scss */"./resources/sass/index.scss");
-__webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\sass\footer.scss */"./resources/sass/footer.scss");
-__webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\sass\login.scss */"./resources/sass/login.scss");
-module.exports = __webpack_require__(/*! D:\xampp.v2\htdocs\drazamed\drazamed\resources\sass\drazamed.scss */"./resources/sass/drazamed.scss");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/header.scss */"./resources/sass/header.scss");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/index.scss */"./resources/sass/index.scss");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/footer.scss */"./resources/sass/footer.scss");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/login.scss */"./resources/sass/login.scss");
+__webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/drazamed.scss */"./resources/sass/drazamed.scss");
+module.exports = __webpack_require__(/*! /home/astagg/work/project-drazamed/drazamed/resources/sass/search-form.scss */"./resources/sass/search-form.scss");
 
 
 /***/ })
