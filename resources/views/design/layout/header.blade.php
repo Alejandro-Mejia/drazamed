a650fe6036d8c78358992e476f729935abe28d74
Ubuntu <ubuntu@ip-172-31-38-234.ec2.internal>
2020-08-09 03:33:20 +0000

    Revert "Cambia el boton de Perfil cuando hay login en celular"
    This reverts commit d7dd410737f45ced066a708ab07eca721657dd3a.

--- resources/views/design/layout/header.blade.php
+++ resources/views/design/layout/header.blade.php
@@ -54,26 +54,7 @@
                 <li><a href="/about">Quiénes Somos</a></li>
                 <li><a href="/contact">Contacto</a></li>
                 @if (!Auth::check() )
-                <button
-                class="btn btn-login btn-profile"
-            >
-                <?php
-                    if(Auth::user()->user_type_id==UserType::CUSTOMER())
-                    {
-                        $name=Auth::user()->customer;
-                        $full_name=$name->first_name." ".$name->last_name;
-                        $phone=$name->phone;
-                    }
-                    elseif(Auth::user()->user_type_id==UserType::MEDICAL_PROFESSIONAL())
-                    {
-                        $name=DB::table('ed_professional')->where('prof_mail', Auth::user()->email)->select('prof_first_name','prof_last_name','prof_phone')->first() ;
-                        $full_name=$name->prof_first_name." ".$name->prof_last_name;
-                        $phone=$name->prof_phone;
-                    }
-
-                ?>
-                {{ $full_name ?? '' }}
-            </button>
+                    <button class="u-profile-btn">Perfil</button>
                 @else
                     <button class="m-login-btn" data-toggle="modal" data-target="#login-modal">Ingresar</button>
                 @endif
