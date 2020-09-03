21d2eda6b0b17576206d81e96d57a352a5ea854c
Alejandro Mejia <alejomejia1@gmail.com>
2020-09-03 12:10:42 -0500

    ajustes orden carga librerias

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -14,21 +14,22 @@ try {
 } catch (e) {}
 
 
+// import 'jquery-ui/ui/widgets/autocomplete.js';
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+// /**
+//  * We'll load the axios HTTP library which allows us to easily issue requests
+//  * to our Laravel back-end. This library automatically handles sending the
+//  * CSRF token as a header based on the value of the "XSRF" token cookie.
+//  */
 
-window.axios = require('axios');
-window.lang = require('lang.js');
-
-
-// import 'jquery-ui/ui/widgets/datepicker.js';
+// window.axios = require('axios');
+// window.lang = require('lang.js');
 
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// // import 'jquery-ui/ui/widgets/datepicker.js';
+
+
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -57,4 +58,22 @@ window.Echo = new Echo({
 //     console.log('notif arrived', e);
 // })
 
+// import 'jquery-ui/ui/widgets/autocomplete.js';
+
 import 'jquery-ui/ui/widgets/autocomplete.js';
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+window.lang = require('lang.js');
+
+
+// import 'jquery-ui/ui/widgets/datepicker.js';
+
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
