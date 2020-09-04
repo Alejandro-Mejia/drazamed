bf681dc93a8f8473ff5f29da68771d4d40311e39
Alejandro Mejia <alejomejia1@gmail.com>
2020-09-04 10:14:14 -0500

    correccion ortografia

--- resources/views/design/about.blade.php
+++ resources/views/design/about.blade.php
@@ -658,7 +658,7 @@
     <div class="container justify-content-center" id="galeria p-2">
         <!--<h1 class="display-4 py-4 text-primary text-center">¡Descargala ahora!</h1> -->
         <h1 class="display-4 py-4 text-primary text-center">Nuestra App</h1>
-        <p class="text-center" style="font-size: 2rem"> <!-- <b>Nuestra App</b> --> Próximamente disponble en PlayStore y AppStore, lista para que personalices tu pastillero
+        <p class="text-center" style="font-size: 2rem"> <!-- <b>Nuestra App</b> --> Próximamente disponible en PlayStore y AppStore, lista para que personalices tu pastillero
             <!-- , hables directamente con el doctor --> y programes las próximas entregas de tus medicamentos. </p>
         <div class="row" id="appButtons">
             <div class="col-md-6 col-sm-12 justify-content-center p-3" style="text-align: center">
