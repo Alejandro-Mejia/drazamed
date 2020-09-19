042f44120f3f13f966bd25d1613479ab03a0c4e1
Alejandro Mejia <alejomejia1@gmail.com>
2020-09-19 12:22:33 -0500

    Se adicionan headers a preflight

--- app/Http/Middleware/PreflightResponse.php
+++ app/Http/Middleware/PreflightResponse.php
@@ -13,6 +13,9 @@ class PreflightResponse
     public function handle($request, Closure $next )
     {
         header ("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+
         if ($request->getMethod() === "OPTIONS") {
             return response('');
         }
