1303fd9e58b86ca5d527ea1dc20bb91fcab6cc7a
Alejandro Mejia <alejomejia1@gmail.com>
2020-09-24 14:39:12 -0500

    Se corrige nombre del parametro en get My Cart App

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -1442,7 +1442,7 @@ class MedicineController extends BaseController
 
 	function getMyCart()
 	{
-		$email = Session::get ('user_id');
+		$email = Session::get ('email');
 		$current_orders = DB::table ('sessions')->where ('user_id' , '=' , $email)->get ();
 
 		return View::make ('/design/cart' , array('current_orders' => $current_orders));
