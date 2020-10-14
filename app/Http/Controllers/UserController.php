0507a62b9b7bc90879cc798bec51ecc1fd6aa6c4
Alejandro Mejia <alejomejia1@gmail.com>
2020-10-13 19:34:22 -0500

    Se adicionan los headers a la funcion resetPassword (CORS)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -486,7 +486,8 @@ class UserController extends BaseController
 	 */
 	public function anyResetPassword ()
 	{
-        //header ("Access-Control-Allow-Headers: '*'");
+        header ("Access-Control-Allow-Origin: *");
+        header ("Access-Control-Allow-Headers: '*'");
 		try {
             $email = Request::get ('email' , '');
 
