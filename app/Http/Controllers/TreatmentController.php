00a07b1e755c2aaf8d74284687e7cc71991dc6c1
Alejandro Mejia <alejomejia1@gmail.com>
2021-01-17 17:55:31 -0500

    conversion de user a array

--- app/Http/Controllers/TreatmentController.php
+++ app/Http/Controllers/TreatmentController.php
@@ -62,7 +62,7 @@ class TreatmentController extends Controller
 
             $user = Customer::where('id', '=', $treatment["customer_id"])->first();
             error_log($user);
-            Log::info(json_decode($user));
+            Log::info($user->toArray());
 
             if ($user["token"] != "") {
                 // Log::info("Verificando token");
