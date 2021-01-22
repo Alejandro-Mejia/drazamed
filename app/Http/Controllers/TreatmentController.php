c5421a3d629d704adf8837d2bf998049903011a9
Alejandro Mejia <alejomejia1@gmail.com>
2021-01-21 20:19:53 -0500

    Se cambia temporalmente a push_notification_dev

--- app/Http/Controllers/TreatmentController.php
+++ app/Http/Controllers/TreatmentController.php
@@ -587,7 +587,7 @@ class TreatmentController extends Controller
         curl_setopt($http2ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
 
         // send push
-        $apple_cert = 'push_notification.p12';
+        $apple_cert = 'push_notification_dev.p12';
         $message = '{"aps":{"alert":{"title": $title, "body":$body},"sound":"default"}}';
         $token = 'e63bce390702b9648d5f46c15e1a7e18f67b3ac38bb5795903cbc93eb75798fb';
         // $token = $device_id;
