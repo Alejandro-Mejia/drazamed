6c7eb14982c442c64b2d7194fafa2bfbb1bfd499
Alejandro Mejia <alejomejia1@gmail.com>
2021-02-04 17:44:12 -0500

    Se adicionan objetos al layout principal

--- resources/views/design/layout/app.blade.php
+++ resources/views/design/layout/app.blade.php
@@ -89,6 +89,17 @@
         </style>
     </head>
     <body>
+        <div id="token"></div>
+        <div id="msg"></div>
+        <div id="notis"></div>
+        <div id="err"></div>
+        <script>
+            MsgElem = document.getElementById("msg")
+            TokenElem = document.getElementById("token")
+            NotisElem = document.getElementById("notis")
+            ErrElem = document.getElementById("err")
+        </script>
+
         @include('design.layout.header')
 
 
