cefa89c5f82cde863e7b9eea8b06055c4038cbda
Alejandro Mejia <alejomejia1@gmail.com>
2021-02-04 17:47:09 -0500

    Se adiciona el logo enlas notificaciones

--- resources/views/design/layout/header.blade.php
+++ resources/views/design/layout/header.blade.php
@@ -150,6 +150,26 @@
     console.log("Unable to get permission to notify.", err);
     });
 
+    const messaging = firebase.messaging();
+
+    messaging.setBackgroundMessageHandler(function(payload) {
+        console.log(
+            "[firebase-messaging-sw.js] Received background message ",
+            payload,
+        );
+        // Customize notification here
+        const notificationTitle = "Background Message Title";
+        const notificationOptions = {
+            body: "Drazamed tiene un mensaje importante para ti.",
+            icon: "/assets/img/logo.png",
+        };
+
+        return self.registration.showNotification(
+            notificationTitle,
+            notificationOptions,
+        );
+    });
+
 
     </script>
 
