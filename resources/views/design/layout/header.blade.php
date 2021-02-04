85ff15d4d773fd7033d0c422d711d919d7d720c3
Alejandro Mejia <alejomejia1@gmail.com>
2021-02-04 18:09:46 -0500

    Se cambia de posicion la inicializacion de firebase

--- resources/views/design/layout/header.blade.php
+++ resources/views/design/layout/header.blade.php
@@ -127,36 +127,71 @@
 
     <script src="/firebase-messaging-sw.js"> </script>
     <script>
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    // var firebaseConfig = {
-    //     apiKey: "AIzaSyBvFM0v-DCmxGBYwVU-Fi6r_rUkQRBi57U",
-    //     authDomain: "drazamedapp.firebaseapp.com",
-    //     databaseURL: "https://drazamedapp-default-rtdb.firebaseio.com",
-    //     projectId: "drazamedapp",
-    //     storageBucket: "drazamedapp.appspot.com",
-    //     messagingSenderId: "193162804196",
-    //     appId: "1:193162804196:web:5514e23878a8fb473425f1",
-    //     measurementId: "G-YQJ9QT2Y8Z"
-    // };
-    // Initialize Firebase
-    // firebase.initializeApp(firebaseConfig);
-    // firebase.analytics();
+        var firebaseConfig = {
+            apiKey: "AIzaSyBvFM0v-DCmxGBYwVU-Fi6r_rUkQRBi57U",
+            authDomain: "drazamedapp.firebaseapp.com",
+            databaseURL: "https://drazamedapp-default-rtdb.firebaseio.com",
+            projectId: "drazamedapp",
+            storageBucket: "drazamedapp.appspot.com",
+            messagingSenderId: "193162804196",
+            appId: "1:193162804196:web:5514e23878a8fb473425f1",
+            measurementId: "G-YQJ9QT2Y8Z"
+        };
 
-    // const messaging = firebase.messaging();
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+
+        // Retrieve an instance of Firebase Messaging so that it can handle background
+        // messages.
+        const messaging = firebase.messaging();
+
+        // messaging.setBackgroundMessageHandler(function(payload) {
+        //     console.log(
+        //         "[firebase-messaging-sw.js] Received background message ",
+        //         payload,
+        //     );
+        //     // Customize notification here
+        //     const notificationTitle = "Background Message Title";
+        //     const notificationOptions = {
+        //         body: "Drazamed tiene un mensaje importante para ti.",
+        //         icon: "//assets/img/logo.png",
+        //     };
+
+        //     return self.registration.showNotification(
+        //         notificationTitle,
+        //         notificationOptions,
+        //     );
+        // });
+
+        //  const messaging = firebase.messaging();
+        messaging
+        .requestPermission()
+        .then(function () {
+            MsgElem.innerHTML = "Notification permission granted."
+            console.log("Notification permission granted.");
+
+            // get the token in the form of promise
+            return messaging.getToken()
+        })
+        .then(function(token) {
+            // print the token on the HTML page
+            TokenElem.innerHTML = "token is : " + token
+            console.log("Token FCM: " . token);
+        })
+        .catch(function (err) {
+        ErrElem.innerHTML = ErrElem.innerHTML + "; " + err
+        console.log("Unable to get permission to notify.", err);
+        });
+
+        messaging.onMessage(function(payload) {
+            console.log("Message received. ", payload);
+            // ...
+        });
 
 
     </script>
 
-    {{-- <script>
-        var config = {
-            messagingSenderId: "193162804196",
-            apiKey: "AIzaSyBvFM0v-DCmxGBYwVU-Fi6r_rUkQRBi57U",
-            projectId: "drazamedapp",
-            appId: "1:193162804196:web:5514e23878a8fb473425f1"
-        };
-        firebase.initializeApp(config);
-    </script> --}}
+
 
 
 </header>
