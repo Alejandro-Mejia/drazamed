5d4f43af7cc32c5e0dc990358ae8d239890e9fa6
Alejandro Mejia <alejomejia1@gmail.com>
2021-02-04 18:03:22 -0500

    correccion error importscripts

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,15 @@
-importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js");
-importScripts(
-    "https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js",
-);
-// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
-importScripts(
-    "https://www.gstatic.com/firebasejs/8.2.5/firebase-analytics.js",
-);
+if( 'undefined' === typeof window){
+    importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js");
+    importScripts(
+        "https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js",
+    );
+    // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+    importScripts(
+        "https://www.gstatic.com/firebasejs/8.2.5/firebase-analytics.js",
+    );
+ }
+
+
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
