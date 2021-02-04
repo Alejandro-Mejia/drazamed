433b8166dacb46c9c7f432637f28256bbcec5264
Alejandro Mejia <alejomejia1@gmail.com>
2021-02-04 17:53:46 -0500

    Cambios en el orden de inicialiacion de firemessaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,7 +18,7 @@
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler(function(payload) {
     console.log(
--- resources/views/design/layout/header.blade.php
+++ resources/views/design/layout/header.blade.php
@@ -138,26 +138,6 @@
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
 
-    const messaging = firebase.messaging();
-    messaging
-    .requestPermission()
-    .then(function () {
-        MsgElem.innerHTML = "Notification permission granted."
-        console.log("Notification permission granted.");
-
-        // get the token in the form of promise
-        return messaging.getToken()
-    })
-    .then(function(token) {
-        // print the token on the HTML page
-        TokenElem.innerHTML = "token is : " + token
-        console.log("Token FCM: " . token);
-    })
-    .catch(function (err) {
-    ErrElem.innerHTML = ErrElem.innerHTML + "; " + err
-    console.log("Unable to get permission to notify.", err);
-    });
-
     // const messaging = firebase.messaging();
 
 
